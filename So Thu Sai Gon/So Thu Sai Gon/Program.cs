71dbddf367e0b8a31ea101c89420469c6b58813a
htlna <nahoang044@gmail.com>
2017-10-04 21:43:51 +0700

    xin chào
    xin chào

---
+++ So Thu Sai Gon/So Thu Sai Gon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace So_Thu_Sai_Gon
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
